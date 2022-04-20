f5ae9102f2331650795a58887a3f946a4f6fa529
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-20 20:39:51 +0600

    :chore Сделал метод DAO Для шага 1

--- videos_dao.py
+++ videos_dao.py
@@ -2,19 +2,72 @@ from config import *
 import sqlite3
 
 
+# Структура таблицы
+# -----------------------
+# show_id — id тайтла
+# type — фильм или сериал
+# title — название
+# director — режиссер
+# cast — основные актеры
+# country — страна производства
+# date_added — когда добавлен на Нетфликс
+# release_year — когда выпущен в прокат
+# rating — возрастной рейтинг
+# duration — длительность
+# duration_type — минуты или сезоны
+# listed_in — список жанров и подборок
+# description — краткое описание
+# -----------------------
+
 class VideosDao:
 
-    def __init__(self):
-        self.sqlite_query = None
-
-    def get_sqlite_connection(self):
+    def get_sqlite_connection(self, sqlite_query):
         """Делает запрос с нужными параметрами
         параметры надо сформировать другими методами и передать через поле объекта self.sqlite_query"""
         with sqlite3.connect(DATA_BASE) as connection:
             cursor = connection.cursor()
-            sqlite_query = self.sqlite_query
+            sqlite_query = sqlite_query
             result = cursor.execute(sqlite_query)
             return result.fetchall()
 
     def get_last_by_title(self, title):
-        pass
+        """Поиск по названию. Если таких фильмов несколько, выводит самый свежий.
+         Возвращает данные в виде словаря"""
+        title_sqlite = "'" + "%" + str(title) + "%" + "'"
+        sqlite_query = f"""
+                SELECT title, country, MAX(release_year), listed_in, description
+                FROM netflix
+                WHERE title LIKE {title_sqlite}
+                        
+        """
+        result = self.get_sqlite_connection(sqlite_query)
+        result_dict = {}
+
+        if result[0][0] is not None:
+
+            key_list = ['is_search', 'title', 'country', 'release_year', 'genre', 'description']
+            count = 0
+            result_dict = {}
+            for key in key_list: # Это ведь лучше прямого назначения для КАЖДОГО ключа? Типа result_dict['country'] = result[0][1]
+                result_dict[key] = result[0][count]
+                count += 1
+                if count > 4: break
+
+        #     result_dict['is_search'] = True
+        #     result_dict['title'] = result[0][0]
+        #     result_dict['country'] = result[0][1]
+        #     result_dict['release_year'] = result[0][2]
+        #     result_dict['genre'] = result[0][3]
+        #     result_dict['description'] = result[0][4].strip('\n')
+        #
+        # else:
+        #     result_dict['is_search'] = False
+
+        return result_dict
+
+
+
+dao = VideosDao()
+name = dao.get_last_by_title("ava")
+print(name)
+
